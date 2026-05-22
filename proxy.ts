6e06fc1054b3e69f5a23e559dd1a4202592e9447
Juan Pedro Vidaia <juanpedro@viajesvidaia.com>
2026-05-22 17:45:30 +0200

    middleware deprecated in Next.js 16

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { ENABLED_LANGUAGES, DEFAULT_LANGUAGE } from '@/lib/config/languages.config'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
   const firstSegment = pathname.split('/')[1]
   if (ENABLED_LANGUAGES.includes(firstSegment)) return NextResponse.next()
